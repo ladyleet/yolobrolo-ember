c79a90b6a2b232bcd550316803f0414da32cb6da
Tracy Lee <tracy@wingman.events>
2016-08-08 17:32:54 -0700

    finish the app

--- app/router.js
+++ app/router.js
@@ -2,10 +2,15 @@ import Ember from 'ember';
 import config from './config/environment';
 
 const Router = Ember.Router.extend({
-  location: config.locationType
+  location: config.locationType,
+  rootURL: config.rootURL
 });
 
 Router.map(function() {
+  this.route('featured', { path:'/' });
+  this.route('about');
+  this.route('videos');
+  this.route('featured');
 });
 
 export default Router;
